09e398f450610d0ca39a774b9b33514d14e9feff
Livan Helgorion <helgorion.livan@gmail.com>
2025-11-08 13:43:40 +0200

    fix solution

--- app/main.py
+++ app/main.py
@@ -8,21 +8,19 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_instances = []
+    Person.people.clear()
 
-    for person_dict in people:
-        person = Person(person_dict["name"], person_dict["age"])
-        person_instances.append(person)
+    person_instances = [Person(p["name"], p["age"]) for p in people]
 
     for person_dict in people:
         current_person = Person.people[person_dict["name"]]
 
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            wife_name = person_dict["wife"]
+        wife_name = person_dict.get("wife")
+        if wife_name is not None:
             current_person.wife = Person.people[wife_name]
 
-        if "husband" in person_dict and person_dict["husband"] is not None:
-            husband_name = person_dict["husband"]
+        husband_name = person_dict.get("husband")
+        if husband_name is not None:
             current_person.husband = Person.people[husband_name]
 
     return person_instances
